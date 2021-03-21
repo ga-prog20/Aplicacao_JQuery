888994a968cd406e4888f531203f99d002bbaaf1
Gabriel Cordeiro <gabrielcordeiro66@gmail.com>
2021-03-21 20:12:49 -0300

    Alterando o JQuery

--- script_jquery.js
+++ script_jquery.js
@@ -1,15 +1,13 @@
 $(function(){
     $('#azul').click(() => {
             $('p').css("color", "blue");
-            $('p').fadeOut();
-            $('p').delay(1000);
-            $('p').fadeIn('slow');
+            $('p').fadeOut(2000);
+            $('p').fadeIn('fast');
         });
     $('#vermelho').click(() => {
             $('p').css("color", "red");
-            $('p').fadeOut();
-            $('p').delay(1000);
-            $('p').fadeIn('slow');
+            $('p').fadeOut(3000);
+            $('p').fadeIn(5000);
         });
     
 });
