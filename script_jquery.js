241bfe7e707d949053cc2f2807d6a524282b7ccd
Gabriel Cordeiro <gabrielcordeiro66@gmail.com>
2021-03-21 21:31:25 -0300

    Finalizando o JQuery

--- script_jquery.js
+++ script_jquery.js
@@ -1,13 +1,50 @@
 $(function(){
     $('#azul').click(() => {
-            $('p').css("color", "blue");
-            $('p').fadeOut(2000);
-            $('p').fadeIn('fast');
+            $('p').css("color", "blue")
+           .fadeOut(2000)
+           .fadeIn('fast');
         });
     $('#vermelho').click(() => {
-            $('p').css("color", "red");
-            $('p').fadeOut(3000);
-            $('p').fadeIn(5000);
+            $('p').css("background-color", "red");
+            $('#mensagem')
+            .text("Cor alterada com sucesso")
+            .css({color:'red', border:'1px solid red'})     
+            .delay(3000)
+            .fadeOut('fast')
+            .addClass('green')
+            
+             $('button').removeClass('red');
+
+        });
+        $(function(){
+            
+            $('#l1').click(() => {
+                $('#i1').show();
+                $('#i2').hide();
+                $('#i3').hide();
+                $('#i4').hide();
+            });
+            
+            $('#l2').click(() => {
+                $('#i1').hide();
+                $('#i2').show();
+                $('#i3').hide();
+                $('#i4').hide();
+
+            });
+            $('#l3').click(() => {
+                $('#i1').hide();
+                $('#i2').hide();
+                $('#i3').show();
+                $('#i4').hide();
+            });
+            $('#l4').click(() => {
+                $('#i1').hide();
+                $('#i2').hide();
+                $('#i3').hide();
+                $('#i4').show();
+            });
+
         });
     
 });
